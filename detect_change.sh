2f84633a29014cb652209f541250f846c89e6b15
Janne Palo <73587747+paloj@users.noreply.github.com>
2022-04-29 00:07:03 +0300

    Update detect_change.sh

--- detect_change.sh
+++ detect_change.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 while inotifywait -e modify -e delete /home/pi/infokuvat/; do
-    echo "Muutos havaittu. Käynnistetään vlc uudelleen";
+    echo "File change detected. Restarting vlc...";
     killall vlc;
     sleep 2;
     cmd="cvlc --quiet --x11-display :0 -fL /home/pi/infopics/*";
